2dbf45c40feb239a9c66d4d92129a482ea68e155
JudLup Luna <judlup@gmail.com>
2017-11-08 20:23:24 -0500

    Test SPI

---
+++ Tester_de_NRF24L01_.ino
@@ -0,0 +1,16 @@
+// nRF24L01+ Active Test
+#include <SPI.h>
+#include <RF24.h>
+#include "printf.h"
+#define RF_CS 9
+#define RF_CSN 10
+RF24 radio(RF_CS, RF_CSN);
+void setup() {
+  Serial.begin(9600);
+  printf_begin();
+  radio.begin();
+  radio.printDetails();
+}
+void loop() {
+
+}
